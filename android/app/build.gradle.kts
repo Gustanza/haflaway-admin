1b8069d56cfe81eb91f8ad47bb70b4213cc81a33
Dennis Genya <genyadennis91@gmail.com>
2025-09-26 15:13:32 +0300

    Initial commit for Haflaway Super Admin app

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.haflaway.adminapp"
+    namespace = "com.haflaway.superadminapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"//flutter.ndkVersion
 
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.haflaway.adminapp"
+        applicationId = "com.haflaway.superadminapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion//flutter.minSdkVersion
--- android/app/src/main/kotlin/com/haflaway/superadminapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/haflaway/superadminapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.haflaway.adminapp
+package com.haflaway.superadminapp
 
 import io.flutter.embedding.android.FlutterActivity
 
