c3d1ff93c6375c428039b7b2412d4dc12a678e88
gustanza <gustanzasam@gmail.com>
2026-03-25 10:03:05 +0300

    ASDMMM

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class HfApp extends StatelessWidget {
     // } else {
     return Center(
       child: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 720),
+        constraints: BoxConstraints(maxWidth: 500),
         child: MaterialApp(
           home: const SplashScreen(),
           debugShowCheckedModeBanner: false,
